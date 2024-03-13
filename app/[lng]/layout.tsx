5b7ca6d154c9891d8654d04ad479a2b757a90673
alsheroziy <raxmatshehroz@.com>
2024-03-13 23:53:06 +0500

    start

---
+++ app/[lng]/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from 'next'
+import { Roboto, Space_Grotesk as SpaceGrotesk } from 'next/font/google'
+import './globals.css'
+import { ChildProps } from '@/types'
+import { ThemeProvider } from '@/components/providers/theme.provider'
+import { languages } from '@/i18n/settings'
+import { dir } from 'i18next'
+
+const roboto = Roboto({
+	subsets: ['latin', 'cyrillic'],
+	weight: ['100', '300', '400', '500', '700', '900'],
+	variable: '--font-roboto',
+})
+
+const spaceGrotesk = SpaceGrotesk({
+	weight: ['300', '400', '500', '600', '700'],
+	subsets: ['latin'],
+	variable: '--font-space-grotesk',
+})
+
+export async function generateStaticParams() {
+	return languages.map(lng => ({ lng }))
+}
+
+export const metadata: Metadata = {
+	title: 'Startup Praktikum - Next.js',
+	description: "Startup Praktikum's Next.js project",
+	icons: { icon: '/logo.svg' },
+}
+
+interface Props extends ChildProps {
+	params: { lng: string }
+}
+
+function RootLayout({ children, params: { lng } }: Props) {
+	return (
+		<html lang={lng} dir={dir(lng)} suppressHydrationWarning>
+			<body
+				className={`${roboto.variable} ${spaceGrotesk.variable} overflow-x-hidden`}
+				suppressHydrationWarning
+			>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	)
+}
+
+export default RootLayout
---
+++ components/shared/language-dropdown.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { Languages } from 'lucide-react'
+import { Button } from '../ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import Image from 'next/image'
+import { lngs } from '@/constants'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { useParams } from 'next/navigation'
+
+function LanguageDropdown() {
+	const { lng } = useParams()
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant='ghost' size={'icon'}>
+					<Languages />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className='w-56'>
+				<DropdownMenuGroup>
+					{lngs.map(item => (
+						<Link key={item.route} href={`/${item.route}`}>
+							<DropdownMenuItem
+								className={cn(
+									'cursor-pointer',
+									lng === item.route && 'bg-secondary'
+								)}
+							>
+								<Image
+									src={`/assets/locales/${item.route}.png`}
+									alt={item.label}
+									width={30}
+									height={30}
+								/>
+								<span className='ml-2 font-space-grotesk font-medium'>
+									{item.label}
+								</span>
+							</DropdownMenuItem>
+						</Link>
+					))}
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
+
+export default LanguageDropdown
